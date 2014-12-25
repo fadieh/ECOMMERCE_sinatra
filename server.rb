d41fe8f28e905a7b411c2200bb04d60a480e3977
Fadie Hannona <fadiehannona@gmail.com>
2014-12-25 21:36:40 +0000

    extended previous with flash notice and save attempt

--- server.rb
+++ server.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'data_mapper'
+require 'rack-flash'
 
 env = ENV["RACK_ENV"] || "development"
 
@@ -25,6 +26,8 @@ class Ecommerce < Sinatra::Base
 	enable :sessions
 	set :session_secret, 'super secret'
 
+	use Rack::Flash
+
 	# routing
 
 	get '/' do
@@ -33,15 +36,21 @@ class Ecommerce < Sinatra::Base
 	end
 
 	get '/users/new' do
+		@user = User.new
 		erb :"users/new"
 	end
 
 	post '/users' do
-		user = User.create(:email => params[:email],
+		@user = User.create(:email => params[:email],
 					:password => params[:password],
 					:password_confirmation => params[:password_confirmation])
-		session[:user_id] = user.id
-		redirect to('/')
+		if @user.save
+			session[:user_id] = @user.id
+			redirect to('/')
+		else
+			flash[:notice] = "Sorry, your passwords don't match"
+			erb :"users/new"
+		end
 	end
 
 	run! if app_file == $0
